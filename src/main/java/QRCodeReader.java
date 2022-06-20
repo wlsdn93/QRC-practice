65ea2956fb3e4552b1969da6c0b6b8a1e8e8d399
jinwoo.an <jinwoo.an@8byte.co.kr>
2022-06-20 13:16:22 +0900

    Detector.detect().getPoints()

--- src/main/java/QRCodeReader.java
+++ src/main/java/QRCodeReader.java
@@ -2,8 +2,10 @@ import com.google.zxing.*;
 import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.DetectorResult;
 import com.google.zxing.common.HybridBinarizer;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+import com.google.zxing.qrcode.detector.Detector;
 
 import javax.imageio.ImageIO;
 import java.io.File;
@@ -34,15 +36,27 @@ public class QRCodeReader {
 
         return new MultiFormatReader().decode(binaryBitmap).getText();
     }
+    // point 는 QR Code 의를 구분하는 finder pattern 의 위치를 의미한다.
+    public static void getPoint(String path) throws IOException, NotFoundException, FormatException {
+        Detector detector =
+                new Detector(
+                        new HybridBinarizer(
+                                new BufferedImageLuminanceSource(
+                                        ImageIO.read(new FileInputStream(path)))).getBlackMatrix());
+        DetectorResult detectorResult = detector.detect();
+        for (ResultPoint point : detectorResult.getPoints()) {
+            System.out.println(point);
+        }
+    }
 
     // Driver code
-    public static void main(String[] args) throws WriterException, IOException, NotFoundException {
+    public static void main(String[] args) throws WriterException, IOException, NotFoundException, FormatException {
 
         // The data that the QR code will contain
         String data = "hello222";
 
         // The path where the image will get saved
-        String path = "src/main/resources/sample.png";
+        String path = "src/main/resources/qrcode-with-text.png";
 
         // Encoding charset
         String charset = "UTF-8";
@@ -50,10 +64,12 @@ public class QRCodeReader {
         Map<EncodeHintType, ErrorCorrectionLevel> hint = new HashMap<>();
         hint.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.L);
 
-        createQR(data, path, charset, 185, 185, hint);
-
-        System.out.println(
-                "QRCode output: "
-                        + readQRCode(path));
+//        createQR(data, path, charset, 185, 185, hint);
+//
+//        System.out.println(
+//                "QRCode output: "
+//                        + readQRCode(path));
+//
+        getPoint(path);
     }
 }
